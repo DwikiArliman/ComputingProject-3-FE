82e371b7f5e66fdc37e499fb2eed6aa7de4eb6ad
lalosianturi21 <fulalotio@gmail.com>
2025-04-10 02:06:50 +0700

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,71 @@
+const INFO = {
+	main: {
+		title: "MRI Brain Classification",
+		name: "Tio Fulalo",
+		email: "fulalotio@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/lalosianturi21",
+		linkedin: "https://www.linkedin.com/in/tio-fulalo-simatupang-5b9547210/",
+		instagram: "https://www.instagram.com/lalosianturi21/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://www.facebook.com/lalosianturi21",
+	},
+
+	homepage: {
+		title: "Brain MRI Classification",
+		description:
+			"The Brain MRI classification system I developed can identify four categories: Meningioma, Pituitary, Glioma, and Healthy. Using an AI-powered approach, this model assists in detecting and classifying brain abnormalities with high accuracy, supporting faster and more precise medical diagnoses.",
+	},	
+
+	about: {
+		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Meningioma",
+			description:
+				"Meningioma is a type of brain tumor that develops in the meninges, the protective layers of the brain and spinal cord. Although usually benign, this tumor can cause various neurological symptoms depending on its location.",
+			logo: "https://res.cloudinary.com/dci5b8svu/image/upload/v1743604444/meningioma_sq1xca.jpg",
+			link: "#",
+		},
+	
+		{
+			title: "Glioma",
+			description:
+				"Glioma is a brain tumor that originates from glial cells, which support the central nervous system. This tumor can be benign or malignant and often affects brain function depending on its size and location.",
+			logo: "https://res.cloudinary.com/dci5b8svu/image/upload/v1743604653/glioma_cr1r1f.jpg",
+			link: "#",
+		},
+	
+		{
+			title: "Pituitary",
+			description:
+				"A pituitary tumor develops in the pituitary gland, which regulates various hormones in the body. While mostly benign, this tumor can cause hormonal imbalances and put pressure on surrounding brain structures.",
+			logo: "https://res.cloudinary.com/dci5b8svu/image/upload/v1743604777/pituitary_vwcok3.jpg",
+			link: "#",
+		},
+	
+		{
+			title: "Healthy",
+			description:
+				"The 'Healthy' category includes MRI brain scans that show no abnormalities or tumors. This data is essential in distinguishing normal conditions from abnormal ones in the classification system.",
+			logo: "https://res.cloudinary.com/dci5b8svu/image/upload/v1743604885/normal_ucjpen.jpg",
+			link: "#",
+		},
+	]
+	
+};
+
+export default INFO;
